d52c8ddcd0c92f1a8b04cab77b1decd27babae8d
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2025-06-16 16:56:24 +0530

    Remove Work component from Landing page to streamline layout

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,11 +10,8 @@ export default function Landing() {
     <div className="bg-black min-h-screen px-12">
       <Header />
       <Hero />
-<<<<<<< HEAD
       <Work />
-=======
       <Blogs />
->>>>>>> c3fa1a51e2eb382ad30fd1deecd758d3b20aaa5b
       <Footer />
     </div>
   );
