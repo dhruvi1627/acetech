ed1c60fc4f9e150774c5d548bf85a4d3db31aaf3
Dhruvi Gajjar <gmi.gj.dev.gajjardmk@gmail.com>
2025-06-18 15:59:18 +0530

    Add AboutUs component to About Us page for enhanced content

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutUsHero from "../components/about_us/AboutUsHero";
 import Values from "../components/about_us/Values";
+import AboutUs from "../components/AboutUs";
 
 export default function AboutUsPage() {
   return (
     <>
       <AboutUsHero />
       <Values/>
+      <AboutUs/>
     </>
   );
 }
