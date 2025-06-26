34ebe240a1dc07205513863da711398885cdbc58
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2025-06-26 16:04:49 +0530

    updated layout for common pages

--- src/components/our_expertise/common/HeroSection.jsx
+++ src/components/our_expertise/common/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = ({
   showDecor = false,
 }) => {
   return (
-    <section className="text-white min-h-screen flex items-center w-full">
+    <section className="text-white pb-20 mb-8 lg:px-20">
       <div className="container mx-auto px-6 lg:px-8">
         <div className="grid lg:grid-cols-2 gap-20 items-start pt-20">
           {/* Left Side Content */}
