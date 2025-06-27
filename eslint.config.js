c652b38c3526ad51702333677ec0e90f0b41116d
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2025-06-27 11:27:12 +0530

    updates in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,4 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 
 export default [
   { ignores: ["dist"] },
@@ -19,11 +16,13 @@ export default [
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react, // 👈 Add this
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      ...react.configs.recommended.rules, // 👈 Include React rules
+      "no-unused-vars": ["error", { varsIgnorePattern: "^(motion|[A-Z_])" }],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
